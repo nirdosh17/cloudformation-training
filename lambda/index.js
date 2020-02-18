bf3680ae5e65a7ad7d08c246c7a02904ee256636
Nirdosh <nrdshgtm@gmail.com>
2020-02-18 15:09:02 +0545

    Working code.

--- lambda/index.js
+++ lambda/index.js
@@ -1,4 +1,18 @@
-exports.handler =  function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return
-}
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+    var params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: "data.json",
+    };
+
+    const data = await s3.getObject(params).promise();
+    console.log(`Data: ${data}`)
+
+    const response = {
+        statusCode: 200,
+        body: data.Body.toString(),
+    };
+    return response;
+};
